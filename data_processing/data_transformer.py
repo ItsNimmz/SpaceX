d5af997dd95328b9b62382593ab2166603fdc439
nimisha <nimishanimmz@gmail.com>
2025-03-26 18:37:54 -0400

    launch data cleaned and transformed

---
+++ data_processing/data_transformer.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from datetime import datetime
+
+# Function to Clean and Transform the data
+def clean_and_transform_launch_data(data):
+        
+        # Convert data to DataFrame 
+        df = pd.DataFrame(data)
+
+        # Get basic info about the dataset
+        # print('Information about the dataset\n\n', df.info())
+
+        # Convert mission_id from list to string
+        df['mission_id'] = df['mission_id'].apply(lambda x: x[0] if isinstance(x, list) else x)
+
+        # Extract rocket_id from the dictionary in column I
+        df['rocket_id'] = df['rocket'].apply(lambda x: x['rocket']['id'] if isinstance(x, dict) and 'rocket' in x and isinstance(x['rocket'], dict) else None)
+
+        #  Convert launch_date_utc to datetime
+        df['launch_date_utc'] = pd.to_datetime(df['launch_date_utc'])
+
+        # Remove the timezone information (make datetime timezone-unaware)
+        df['launch_date_utc'] = df['launch_date_utc'].dt.tz_localize(None)
+
+        # Drop columns where all values are NaN (empty columns)
+        df = df.dropna(axis=1, how='all')
+
+        # Drop the unwanted column
+        df = df.drop(columns=['id','rocket'])
+
+        # Drop rows with missing values in any of the specified columns
+        df = df.dropna(subset=['mission_id', 'mission_name', 'rocket_id'])
+
+        df.to_excel('cleaned_launch_data.xlsx', index=False)
+
+        return df
+        
+        
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # Adjust the import statement to account for the 'processing' directory
 from data_processing.extract import fetch_batched_data_with_rate_limiting
+import data_processing.data_transformer as transformation
 
 
 
 # Example usage
 if __name__ == "__main__":
-    launches = fetch_batched_data_with_rate_limiting()
+    launches = fetch_batched_data_with_rate_limiting() #Data extraction
 
     if launches:
-        print(f"Fetched {len(launches)} launches.")
+        print(f"Fetched {len(launches)} launches. Now doing the cleaning and transformations")
+        cleaned_data = transformation.clean_and_transform_launch_data(launches)
+        print(cleaned_data)
